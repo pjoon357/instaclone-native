3662c0a741e24315315fdcdb267280860d88a57c
pjoon357 <pjoon357@naver.com>
2021-08-20 22:09:33 +0900

    feat: Connect http

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,7 +1,33 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+import { Text, TouchableOpacity, View } from "react-native";
+import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../fragments";
+import { logUserOut } from "../apollo";
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      ...PhotoFragment
+      user {
+        username
+        avatar
+      }
+      caption
+      comments {
+        ...CommentFragment
+      }
+      createdAt
+      isMine
+    }
+  }
+  ${PHOTO_FRAGMENT}
+  ${COMMENT_FRAGMENT}
+`;
+
 
 export default function Feed() {
+    const { data } = useQuery(FEED_QUERY);
+    console.log(data);
     return (
         <View
             style={{
@@ -11,7 +37,9 @@ export default function Feed() {
                 justifyContent: "center",
             }}
         >
-            <Text style={{ color: "white" }}>Feed</Text>
+            <TouchableOpacity onPress={() => logUserOut()}>
+                <Text style={{ color: "white" }}>Feed</Text>
+            </TouchableOpacity>
         </View>
     );
 }
