4d22196bc5d0967039d55c638595331b94fcfaf7
pjoon357 <pjoon357@naver.com>
2021-08-26 21:19:45 +0900

    feat: Add Take Photo Page

--- navigators/LoggedInNav.js
+++ navigators/LoggedInNav.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { createStackNavigator } from "@react-navigation/stack";
 import TabsNav from "./TabsNav";
 import UploadNav from "./UploadNav";
+import UploadForm from "../screens/UploadForm";
 
 const Stack = createStackNavigator();
 
@@ -9,10 +11,32 @@ export default function LoggedInNav() {
     return (
         <Stack.Navigator screenOptions={{
             presentation: "modal",
-            headerShown: false
         }}>
-            <Stack.Screen name="Tabs" component={TabsNav} />
-            <Stack.Screen name="Upload" component={UploadNav} />
+            <Stack.Screen
+                name="Tabs"
+                options={{ headerShown: false }}
+                component={TabsNav}
+            />
+            <Stack.Screen
+                name="Upload"
+                options={{ headerShown: false }}
+                component={UploadNav}
+            />
+            <Stack.Screen
+                name="UploadForm"
+                options={{
+                    headerBackTitleVisible: false,
+                    headerBackImage: ({ tintColor }) => (
+                        <Ionicons color={tintColor} name="close" size={28} />
+                    ),
+                    title: "Upload",
+                    headerTintColor: "white",
+                    headerStyle: {
+                        backgroundColor: "black",
+                    },
+                }}
+                component={UploadForm}
+            />
         </Stack.Navigator>
     );
 }
